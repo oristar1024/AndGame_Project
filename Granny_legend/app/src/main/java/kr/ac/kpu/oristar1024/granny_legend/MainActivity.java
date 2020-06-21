425aad9d927c175fca4205ca23f010b16b715b4e
oristar1024 <tronia94@gmail.com>
2020-06-21 19:19:35 +0900

    add stage buttons
    스테이지 진행도에 따라 TitleView에 스테이지 버튼들이 활성화 된다.

--- Granny_legend/app/src/main/java/kr/ac/kpu/oristar1024/granny_legend/MainActivity.java
+++ Granny_legend/app/src/main/java/kr/ac/kpu/oristar1024/granny_legend/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -12,20 +13,36 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
     private int coins;
     private int level;
+    private int openStage;
+
+    private int[] BUTTON_IDS = {R.id.buttonStage1, R.id.buttonStage2, R.id.buttonStage3,
+                                R.id.buttonStage4, R.id.buttonStage5};
+    private int[] IMAGE_RES_IDS = {R.drawable.stage1, R.drawable.stage2, R.drawable.stage3,
+                                    R.drawable.stage4, R.drawable.boss_stage};
 
     private TextView coinText;
     private TextView levelText;
     private TextView priceText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pref = getSharedPreferences("coin", MODE_PRIVATE);
-
         coins = pref.getInt("coin", 0);
         level = pref.getInt("level", 0);
+        openStage = pref.getInt("stage", 0);
+
+        for(int i = 0; i < 5; ++i){
+            ImageButton button = findViewById(BUTTON_IDS[i]);
+            if(i < openStage) {
+                button.setEnabled(true);
+                button.setBackgroundResource(IMAGE_RES_IDS[i]);
+            }
+            else{
+                button.setEnabled(false);
+            }
+        }
 
         coinText = findViewById(R.id.coinText);
         levelText = findViewById(R.id.levelText);

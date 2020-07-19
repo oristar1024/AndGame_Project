0b00591c116f368d627bda7ea2966ec8136b930b
Kiyong Kim <scgyong.isu@gmail.com>
2020-07-19 11:48:46 +0900

    * 를 + 로 쓴 것 수정

--- Granny_legend/app/src/main/java/kr/ac/kpu/oristar1024/granny_legend/classes/CollisionHelper.java
+++ Granny_legend/app/src/main/java/kr/ac/kpu/oristar1024/granny_legend/classes/CollisionHelper.java
@@ -1,6 +1,7 @@
 package kr.ac.kpu.oristar1024.granny_legend.classes;
 
 import android.graphics.Rect;
+//import android.util.Log;
 
 public class CollisionHelper {
     public static boolean collides(Rect rect, DistanceCollidable obj) {
@@ -12,7 +13,14 @@ public class CollisionHelper {
         float dy = o1.getLocationY() - o2.getLocationY();
         float rsum = o1.getRadius() + o2.getRadius();
 
-        float dist_sq = dx * dx + dy + dy;
-        return dist_sq < rsum * rsum;
+        float dist_sq = dx * dx + dy * dy;
+        if (dist_sq < rsum * rsum) {
+//            String d1 = o1.getLocationX() + ", " + o1.getLocationY() + " <" + o1.getRadius() + "> " + o1;
+//            String d2 = o2.getLocationX() + ", " + o2.getLocationY() + " <" + o2.getRadius() + "> " + o2;
+//            Log.d("Collision", "rsum=" + rsum + " dsq=" + dist_sq + " o1 = " + d1 + " o2 = " + d2);
+            return true;
+        }
+
+        return false;
     }
 }
